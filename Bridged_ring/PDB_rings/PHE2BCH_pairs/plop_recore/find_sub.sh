8cbda1d96097b8d16e926806211744a771858aa8
jgxs <yjcheng02@k226.hn.org>
2022-11-17 16:12:59 +0800

    qsub scripts in z55

---
+++ Bridged_ring/PDB_rings/PHE2BCH_pairs/plop_recore/find_sub.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd /pubhome/yjcheng02/bridge_ring_dataset/Bridged_ring/PDB_rings/PHE2BCH_pairs/lig_bch_complex
+
+ls | while read lig
+    do
+        cd $lig
+        ls | while read pdb
+            do
+                cd $pdb 
+                qsub /pubhome/yjcheng02/bridge_ring_dataset/Bridged_ring/PDB_rings/PHE2BCH_pairs/plop_recore/sub.qsub
+                cd ..
+            done
+        cd ..
+    done
