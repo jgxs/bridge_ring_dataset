64f5da861981aedd3119fc5e600d558a45273488
jgxs <yjcheng02@k226.hn.org>
2022-11-17 15:58:44 +0800

    refine the RLI_plop.sh

--- Bridged_ring/PDB_rings/PHE2BCH_pairs/plop_recore/RLI_plop.sh
+++ Bridged_ring/PDB_rings/PHE2BCH_pairs/plop_recore/RLI_plop.sh
@@ -8,29 +8,34 @@ export LD_LIBRARY_PATH=$plop_path:$LD_LIBRARY_PATH
 PATH=/pubhome/soft/amber/amber20_21/bin:$PATH
 schrodinger_path="/pubhome/yjcheng02/schrodinger2016-2"
 
-interpret="/pubhome/yjcheng02/iso_database/tool/I-interpret/bin/I-interpret"
-interpret_para="/pubhome/yjcheng02/iso_database/tool/I-interpret/bin/parameter.txt"
-
 # the input file contains 3 file. protein structure ends with *sin.pdb; ligand 1 ends with phe.pdb; ligand 2 end with bch.pdb
 pro_file=`ls|grep sin`
 phe_file=`ls|grep phe`
 bch_file=`ls|grep bch`
 
 lig_id=${phe_file:0:3}
-cp "/pubhome/yjcheng02/iso_database/tool/I-interpret/bin/parameter.txt" .
-${interpret} ${phe_file} ${lig_id}_phe.pdb
+sed -i "s/UNL/${lig_id}/g" $phe_file $bch_file
+line=`head -n 1 $phe_file`
+sed -i "s/${line:20:3}/   /g" $phe_file
 
-grep "ATOM  " ${lig_id}_phe.pdb>001.het
+grep "HETATM" $phe_file>001.het
 echo "TER" >> 001.het
 sed -i "s/${lig_id}/001/g" 001.het
 echo 001 > list
 
-antechamber -i 001.het -fi pdb -o 001.mol2 -fo mol2 -c bcc -pf y -at sybyl -nc
-cp 001.het 001.bak
-python /pubhome/yjcheng02/interact/renumber.py --mol 001.mol2 --pdb 001.bak > 001.het
+grep "HETATM" $bch_file > 002.het
+echo "TER" >> 002.het
+sed -i "s/${lig_id}/002/g" 002.het
+echo 002 >> list
 
-$schrodinger_path/utilities/mol2convert -imol2 001.mol2 -omae 001.mae
-$schrodinger_path/utilities/hetgrp_ffgen 2005 001.mae
+for lig in 001 002
+    do
+        antechamber -i ${lig}.het -fi pdb -o ${lig}.mol2 -fo mol2 -c bcc -pf y -at sybyl 
+        cp ${lig}.het ${lig}.bak
+        python /pubhome/yjcheng02/interact/renumber.py --mol ${lig}.mol2 --pdb ${lig}.bak 
+        $schrodinger_path/utilities/mol2convert -imol2 ${lig}.mol2 -omae ${lig}.mae
+        $schrodinger_path/utilities/hetgrp_ffgen 2005 ${lig}.mae
+    done 
 
 echo """rand seed 1111111111
 logfile rec_h_opt.log
@@ -45,10 +50,13 @@ echo -e rec.con| $plop_path/plop
 sed -i "s?plop/6.0?plop/25.1?" *.con
 mkdir log_files
 mkdir out_files
-echo 001.cmxmin.con | /pubhome/yjcheng02/plop25.1/plop
-echo 001.ligmin.con | /pubhome/yjcheng02/plop25.1/plop
-/pubhome/yjcheng02/interact/plop_tool/extract_lig.pl out_files/001_cmxmin.out out_files/001_cmxmin.lig
-echo 001.ligfix.con | /pubhome/yjcheng02/plop25.1/plop
 
-/pubhome/yjcheng02/interact/plop_tool/nrg_set_2009 001.out rigid
+for lig in 001 002
+do
+echo ${lig}.cmxmin.con | /pubhome/yjcheng02/plop25.1/plop32-static
+echo ${lig}.ligmin.con | /pubhome/yjcheng02/plop25.1/plop32-static
+/pubhome/yjcheng02/interact/plop_tool/extract_lig.pl out_files/001_cmxmin.out out_files/${lig}_cmxmin.lig
+echo ${lig}.ligfix.con | /pubhome/yjcheng02/plop25.1/plop32-static
+done 
+/pubhome/yjcheng02/interact/plop_tool/nrg_set_2009 res.out rigid
 
