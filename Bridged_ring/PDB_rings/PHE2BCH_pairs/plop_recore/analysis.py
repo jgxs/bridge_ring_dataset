4b87535d3655556a5fd326b5639b0066ab55e408
jgxs <yjcheng02@k226.hn.org>
2022-11-29 22:10:04 +0800

    refine rescore

--- Bridged_ring/PDB_rings/PHE2BCH_pairs/plop_recore/analysis.py
+++ Bridged_ring/PDB_rings/PHE2BCH_pairs/plop_recore/analysis.py
@@ -1,15 +1,15 @@
 from pathlib import Path
 
-rescore_dir = Path("/pubhome/yjcheng02/bridge_ring_dataset/Bridged_ring/PDB_rings/PHE2BCH_pairs/lig_bch_refine")
+rescore_dir = Path(".")
+rescore_name = rescore_dir.absolute().name
+print(rescore_name)
 all_res = list(rescore_dir.glob("**/res.out"))
+res_dir = "/pubhome/yjcheng02/bridge_ring_dataset/Bridged_ring/PDB_rings/PHE2BCH_pairs/lig_bch_res/"
 
-with open("rescore.log","w") as log:
-    log.write("analysis starting \n")
-with open("rescore.err","w") as log:
-    log.write("analysis starting \n")
-with open("rescore.better","w") as log:
-    log.write("")
-
+Herr = []
+Eerr = []
+better = []
+normol = []
 for res in all_res:
     lig = res.parts[-3]
     pdb = res.parts[-2]
@@ -17,14 +17,19 @@ for res in all_res:
         info = infom.read().split("\n")
     phe_score = float(info[0].split()[1])
     bch_score = float(info[1].split()[1])
-    if phe_score >= 0.0 or bch_score >= 0.0:
-        with open("rescore.err","a") as err:
-            err.write(f"Error of {lig}_{pdb}\n")
+    if phe_score > 0.0 or bch_score > 0.0:
+        Herr.append(f"{lig} {pdb} {phe_score: >12.4f} {bch_score: >12.4f} {bch_score-phe_score: >12.4f}\n")
+    elif phe_score == 0.0 or bch_score == 0.0:
+        Eerr.append(f"Error of molecule ./{lig}/{pdb} \n")
     else:
-        with open("rescore.log","a") as log:
-            log.write(f"{lig} {pdb} {phe_score: >12.4f} {bch_score: >12.4f} {bch_score-phe_score: >12.4f}\n")
-        if bch_score-phe_score < 0:
-            with open("rescore.better","a") as log:
-                log.write(f"{lig} {pdb} {phe_score: >12.4f} {bch_score: >12.4f} {bch_score-phe_score: >12.4f}\n")
-     
+        normol.append(f"{lig} {pdb} {phe_score: >12.4f} {bch_score: >12.4f} {bch_score-phe_score: >12.4f}\n")
+        if bch_score < phe_score:
+            better.append(f"{lig}_{pdb} {phe_score: >12.4f} {bch_score: >12.4f} {bch_score-phe_score: >12.4f}\n")
+
+outlog = {"log":normol,"Herr":Herr,"Eerr":Eerr,"better":better}
+print(f"result is in {res_dir}")
+for key in outlog:
+    with open(f"{res_dir}{rescore_name}.{key}","w") as log:
+        for line in outlog[key]:
+            log.write(line)
 
--- Bridged_ring/PDB_rings/PHE2BCH_pairs/plop_recore/find_sub.sh
+++ Bridged_ring/PDB_rings/PHE2BCH_pairs/plop_recore/find_sub.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd /pubhome/yjcheng02/bridge_ring_dataset/Bridged_ring/PDB_rings/PHE2BCH_pairs/lig_bch_refine
+cd /pubhome/yjcheng02/bridge_ring_dataset/Bridged_ring/PDB_rings/PHE2BCH_pairs/lig_bch_est
 
 ls | while read lig
     do
@@ -9,6 +9,7 @@ ls | while read lig
             do
                 cd $pdb 
                 qsub /pubhome/yjcheng02/bridge_ring_dataset/Bridged_ring/PDB_rings/PHE2BCH_pairs/plop_recore/sub.qsub
+                #echo test
                 cd ..
             done
         cd ..
